c184ad4695cce74fb4a05ff95f8b81ebdcb1f578
ingobecker <mail@ingobecker.de>
2024-01-23 16:22:38 +0100

    Initial Commit

---
+++ Calendar.java
@@ -0,0 +1,9 @@
+public class Calendar {
+    public static void main(String[] args) {
+
+        System.out.println("Januar");
+        System.out.println("Februar");
+        System.out.println("Maerz");
+
+    }
+}
